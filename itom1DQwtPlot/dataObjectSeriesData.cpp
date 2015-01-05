5c48534abf9ed27e35b90fc4008056f5702f7cde
Marc Gronle <gronle@ito.uni-stuttgart.de>
2015-01-05 19:58:26 +0100

    rounding fix in itom1DQwtPlot in order to show the right data to given z-stack-cut coordinates.

--- itom1DQwtPlot/dataObjectSeriesData.cpp
+++ itom1DQwtPlot/dataObjectSeriesData.cpp
@@ -543,8 +543,8 @@ RetVal DataObjectSeriesData::updateDataObject(const ito::DataObject* dataObj, QV
             else
             {
                 m_d.valid = true;
-                pxX1 = dataObj->getPhysToPix(dims - 1, bounds[0].x(), _unused);
-                pxY1 = dataObj->getPhysToPix(dims - 2, bounds[0].y(), _unused);
+                pxX1 = qRound(dataObj->getPhysToPix(dims - 1, bounds[0].x(), _unused));
+                pxY1 = qRound(dataObj->getPhysToPix(dims - 2, bounds[0].y(), _unused));
 
                 saturation( pxX1, 0, dataObj->getSize(dims - 1) - 1 );
                 saturation( pxX2, 0, dataObj->getSize(dims - 1) - 1 );
