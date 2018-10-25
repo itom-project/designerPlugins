788b614323126ea1634119fe44cf328d99c79e70
Johann Krauter <krauter@ito.uni-stuttgart.de>
2018-10-25 05:28:22 +0200

    InsertNextTupleValue name has changed to InsertNextTypedTuple for VTK version > 7

--- vtk3dVisualizer/itemPolygonMesh.cpp
+++ vtk3dVisualizer/itemPolygonMesh.cpp
@@ -696,7 +696,11 @@ void ItemPolygonMesh::setFaceColor(const QColor& color)
             int nrOfPoints = m_mesh.polygonMesh()->cloud.height * m_mesh.polygonMesh()->cloud.width;
             for (int i = 0; i < nrOfPoints; i++)
             {
-                colors->InsertNextTupleValue(ucharColor);
+                #if VTK_MAJOR_VERSION < 7 //VTK version < 7 the name is changed in newer versions of VTK. https://github.com/PointCloudLibrary/pcl/issues/2060
+                    colors->InsertNextTupleValue(ucharColor);
+                #else
+                    colors->InsertNextTypedTuple(ucharColor);
+                #endif
             }
 
             colors->SetName ("SolidColor");
