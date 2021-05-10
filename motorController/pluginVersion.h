faa89062853c00b57c3c61d1fcd19a858e97cd3d
M. Gronle <marc.gronle@gmx.de>
2021-05-10 23:26:15 +0200

    motorController: bugfix when getting numaxis parameter from any actuator plugin

--- motorController/pluginVersion.h
+++ motorController/pluginVersion.h
@@ -32,12 +32,12 @@
 
 #define PLUGIN_VERSION_MAJOR 1
 #define PLUGIN_VERSION_MINOR 2
-#define PLUGIN_VERSION_PATCH 0
+#define PLUGIN_VERSION_PATCH 1
 #define PLUGIN_VERSION_REVISION 0
 #define PLUGIN_VERSION        CREATE_VERSION(PLUGIN_VERSION_MAJOR,PLUGIN_VERSION_MINOR,PLUGIN_VERSION_PATCH)
 #define PLUGIN_VERSION_STRING CREATE_VERSION_STRING(PLUGIN_VERSION_MAJOR,PLUGIN_VERSION_MINOR,PLUGIN_VERSION_PATCH)
 #define PLUGIN_COMPANY        "Institut fuer Technische Optik, University Stuttgart"
-#define PLUGIN_COPYRIGHT        "(C) 2019, ITO, University Stuttgart"
+#define PLUGIN_COPYRIGHT        "(C) 2021, ITO, University Stuttgart"
 #define PLUGIN_NAME           "motorController"
 
 //----------------------------------------------------------------------------------------------------------------------------------
