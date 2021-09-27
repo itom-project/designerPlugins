d851a3588dabeba911cacb4f3e51395ba54ed4b6
M. Gronle <marc.gronle@gmx.de>
2021-09-27 23:55:51 +0200

    itom1dqwtplot, itom2dqwtplot: version number incremented due to smaller adaptions for Qwt 6.2

--- itom1DQwtPlot/pluginVersion.h
+++ itom1DQwtPlot/pluginVersion.h
@@ -1,7 +1,7 @@
 /* ********************************************************************
     itom software
     URL: http://www.uni-stuttgart.de/ito
-    Copyright (C) 2020, Institut fuer Technische Optik (ITO),
+    Copyright (C) 2021, Institut fuer Technische Optik (ITO),
     Universitaet Stuttgart, Germany
 
     This file is part of itom and its software development toolkit (SDK).
@@ -32,7 +32,7 @@
 
 #define PLUGIN_VERSION_MAJOR 3
 #define PLUGIN_VERSION_MINOR 9
-#define PLUGIN_VERSION_PATCH 5
+#define PLUGIN_VERSION_PATCH 6
 #define PLUGIN_VERSION_REVISION 0
 #define PLUGIN_VERSION        CREATE_VERSION(PLUGIN_VERSION_MAJOR,PLUGIN_VERSION_MINOR,PLUGIN_VERSION_PATCH)
 #define PLUGIN_VERSION_STRING CREATE_VERSION_STRING(PLUGIN_VERSION_MAJOR,PLUGIN_VERSION_MINOR,PLUGIN_VERSION_PATCH)
